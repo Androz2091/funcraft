fca6c55e4e9f5227ab8fc287b75a407dcf8913b7
Androz2091 <androz2091@gmail.com>
2020-03-17 17:11:40 +0100

    feat: implement leaderboard

--- structures/Leaderboard.js
+++ structures/Leaderboard.js
@@ -1,8 +1,17 @@
 module.exports = class Leaderboard {
 
     constructor($){
-        //const podium = $(".spodium.game-ulbg-rush").get(0);
-        //console.log(podium);
+
+        const gameCount = $(".name").length;
+
+        for(const i = 0; i < gameCount; i++){
+            const game = {
+                username: $(".name").get(i).children[0].data.trim(),
+                avatar: $(".head").get(i).children[1].attribs.src
+            };
+            const gameName = $(".spodium").get(i).attribs.class.split("ulbg-")[1];
+            this[gameName] = game;
+        }
     }
 
 }
