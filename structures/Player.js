15b1b7ca4c1e977d3e4d0999f4b6dfa0af61c46c
Hunam6 <elietattevindrevet@gmail.com>
2020-03-18 19:33:53 +0100

    Code reorganization

--- structures/Player.js
+++ structures/Player.js
@@ -1,11 +1,11 @@
 const RushStats = require("./RushStats");
-const SkywarsStats = require("./SkywarsStats");
 const HikaBrainStats = require("./HikaBrainStats");
+const SkywarsStats = require("./SkywarsStats");
 const OctogoneStats = require("./OctogoneStats");
-const BlitzStats = require("./BlitzStats");
 const ShootCraftStats = require("./ShootCraftStats");
 const InfecteStats = require("./InfecteStats");
 const SurvivalStats = require("./SurvivalStats");
+const BlitzStats = require("./BlitzStats");
 const PvPSmashStats = require("./PvPSmashStats");
 const LandRushStats = require("./LandRushStats");
 
@@ -23,16 +23,16 @@ module.exports = class Player {
         this.gloryCount = parseInt($(".info-stats").get(0).children[1].children[2].data.trim().split(" Gloires")[0].split(" ").join(""));
         this.gameCount = parseInt($(".info-stats").get(0).children[3].children[0].data.trim().split(" ")[0]);
 
-        this.banned = new Boolean($(".player-alert").get(0));
+        this.banned = Boolean($(".player-alert").get(0));
 
         this.rush = new RushStats($);
-        this.skywars = new SkywarsStats($);
         this.hikaBrain = new HikaBrainStats($);
+        this.skywars = new SkywarsStats($);
         this.octogone = new OctogoneStats($);
-        this.blitz = new BlitzStats($);
         this.shootCraft = new ShootCraftStats($);
         this.infecte = new InfecteStats($);
         this.survival = new SurvivalStats($);
+        this.blitz = new BlitzStats($);
         this.pvPSmash = new PvPSmashStats($);
         this.landRush = new LandRushStats($);
     }
