00eb924ca67c8b260487046c87d0ac12c4f3313c
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-22 13:58:52 +0100

    Update BlitzStats.js

--- structures/BlitzStats.js
+++ structures/BlitzStats.js
@@ -10,13 +10,18 @@ module.exports = class BlitzStats {
         this.gameCount = parseInt(stats.children[7].children[3].children[0].data.split(" ").join("")) || 0;
         this.victoryCount = parseInt(stats.children[9].children[3].children[0].data.split(" ").join("")) || 0;
         this.defeatCount = parseInt(stats.children[11].children[3].children[0].data.split(" ").join("")) || 0;
-        this.wl = parseInt(this.victoryCount / this.defeatCount) || 0;
         this.gameTime = ms(stats.children[13].children[3].children[0].data) || 0;
         this.killCount = parseInt(stats.children[15].children[3].children[0].data.split(" ").join("")) || 0;
         this.deathCount = parseInt(stats.children[17].children[3].children[0].data.split(" ").join("")) || 0;
-        this.kd = parseInt(this.killCount/this.deathCount) || 0;
         this.nexusDamageCount = parseInt(stats.children[19].children[3].children[0].data.split(" ").join(" ")) || 0;
 
+    }
+       get kd() {
+        return parseFloat(this.killCount/this.deathCount) || 0;
+    }
+    
+      get wl() {
+       return parseFloat (this.victoryCount / this.defeatCount) || 0;
     }
 
 };
