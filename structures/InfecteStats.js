66590b5cd3d91bc7ca852dad12fd24e037b1aeca
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-22 12:28:36 +0100

    Update InfecteStats.js

--- structures/InfecteStats.js
+++ structures/InfecteStats.js
@@ -9,9 +9,11 @@ module.exports = class InfecteStats {
         this.points = parseInt(stats.children[5].children[3].children[0].data.split(" ").join("")) || 0;
         this.gameCount = parseInt(stats.children[7].children[3].children[0].data.split(" ").join("")) || 0;
         this.victoryCount = parseInt(stats.children[9].children[3].children[0].data.split(" ").join("")) || 0;
+        this.defeatCount = parseInt(this.gameCount - this.victoryCount) || 0;
         this.gameTime = ms(stats.children[11].children[3].children[0].data) || 0;
         this.killCount = parseInt(stats.children[13].children[3].children[0].data.split(" ").join("")) || 0;
         this.deathCount = parseInt(stats.children[15].children[3].children[0].data.split(" ").join("")) || 0;
+        this.kd = parseInt(this.killCount/this.deathCount) || 0;
 
     }
 
