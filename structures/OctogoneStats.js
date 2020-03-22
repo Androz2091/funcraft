618021d6336c105fa9a7a2a48f46748d1702d6a2
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-22 14:02:35 +0100

    Update OctogoneStats.js

--- structures/OctogoneStats.js
+++ structures/OctogoneStats.js
@@ -9,14 +9,20 @@ module.exports = class OctogoneStats {
         this.points = parseInt(stats.children[5].children[3].children[0].data.split(" ").join("")) || 0;
         this.gameCount = parseInt(stats.children[7].children[3].children[0].data.split(" ").join("")) || 0;
         this.top1Count = parseInt(stats.children[9].children[3].children[0].data.split(" ").join("")) || 0;
-        this.defeatCount = parseInt(this.gameCount - this.top1Count) || 0;
-        this.wl = parseInt(this.top1Count / this.defeatCount) || 0;
         this.gameTime = ms(stats.children[11].children[3].children[0].data) || 0;
         this.killCount = parseInt(stats.children[13].children[3].children[0].data.split(" ").join("")) || 0;
         this.deathCount = parseInt(stats.children[15].children[3].children[0].data.split(" ").join("")) || 0;
-        this.kd = parseInt(this.killCount/this.deathCount) || 0;
         this.damageCount = parseInt(stats.children[17].children[3].children[0].data.split(" ").join("")) || 0;
 
     }
-
+       get kd() {
+        return parseFloat(this.killCount/this.deathCount) || 0;
+    }
+    
+      get wl() {
+       return parseFloat (this.victoryCount / this.defeatCount) || 0;
+    }
+      get defeatCount() {
+       return parseFloat (this.gameCount - this.victoryCount) || 0;
+    }
 };
