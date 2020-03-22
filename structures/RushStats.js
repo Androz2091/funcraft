69f9e1cb6bec2a3d2863e6a97306b4450fa24507
Androz2091 <androz2091@gmail.com>
2020-03-22 16:34:27 +0100

    fix: delete wl property

--- structures/RushStats.js
+++ structures/RushStats.js
@@ -10,11 +10,9 @@ module.exports = class RushStats {
         this.gameCount = parseInt(stats.children[7].children[3].children[0].data.split(" ").join("")) || 0;
         this.victoryCount = parseInt(stats.children[9].children[3].children[0].data.split(" ").join("")) || 0;
         this.defeatCount = parseInt(stats.children[11].children[3].children[0].data.split(" ").join("")) || 0;
-        this.wl = parseInt(this.victoryCount / this.defeatCount) || 0;
         this.gameTime = ms(stats.children[13].children[3].children[0].data) || 0;
         this.killCount = parseInt(stats.children[15].children[3].children[0].data.split(" ").join("")) || 0;
         this.deathCount = parseInt(stats.children[17].children[3].children[0].data.split(" ").join("")) || 0;
-        this.kd = parseInt(this.killCount/this.deathCount) || 0;
         this.destroyedBedCount = parseInt(stats.children[19].children[3].children[0].data.split(" ").join("")) || 0;
 
     }
