5d4ca446d347766b35bacc61c58c434daab478e9
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-22 12:33:21 +0100

    Update SkywarsStats.js

--- structures/SkywarsStats.js
+++ structures/SkywarsStats.js
@@ -9,9 +9,11 @@ module.exports = class SkywarsStats {
         this.points = parseInt(stats.children[5].children[3].children[0].data.split(" ").join("")) || 0;
         this.gameCount = parseInt(stats.children[7].children[3].children[0].data.split(" ").join("")) || 0;
         this.top1Count = parseInt(stats.children[9].children[3].children[0].data.split(" ").join("")) || 0;
+        this.defeatCount = parseint(this.gameCount - this.top1Count) || 0;
         this.gameTime = ms(stats.children[11].children[3].children[0].data) || 0;
         this.killCount = parseInt(stats.children[13].children[3].children[0].data.split(" ").join("")) || 0;
         this.deathCount = parseInt(stats.children[15].children[3].children[0].data.split(" ").join("")) || 0;
+        this.kd = parseInt(this.killCount/this.deathCount) || 0;
 
     }
 
