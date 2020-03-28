b63b51c7305934dde4c618081406bf97843601e0
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-28 17:26:25 +0400

    Update InfecteStats.js

--- structures/InfecteStats.js
+++ structures/InfecteStats.js
@@ -26,11 +26,14 @@ module.exports = class InfecteStats {
     }
 
     get wl() {
-        return (parseFloat(this.victoryCount / this.deathCount) || 0).toFixed(2);
+        return (parseFloat(this.victoryCount / this.defeatCount) || 0).toFixed(2);
     }
 
     get defeatCount() {
         return parseInt(this.gameCount - this.victoryCount) || 0;
     }
 
+    get winrate() {
+        return (parseFloat(this.victoryCount / this.gameCount * 100 || 0).toFixed(2);
+    }
 };
