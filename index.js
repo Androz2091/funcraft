afa8da607ca65cb461e4caec14aa57b42bbb95a4
Hunam <elietattevindrevet@gmail.com>
2020-04-21 19:08:19 +0200

    semicolons from index.js removed
    Signed-off-by: Hunam <elietattevindrevet@gmail.com>

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
+const fetch = require("node-fetch")
+const cheerio = require("cheerio")
 
-const Player = require("./structures/Player");
-const Leaderboard = require("./structures/Leaderboard");
-const PlayerCounts = require("./structures/PlayerCounts");
+const Player = require("./structures/Player")
+const Leaderboard = require("./structures/Leaderboard")
+const PlayerCounts = require("./structures/PlayerCounts")
 
-const baseURL = "https://www.funcraft.net/fr/";
+const baseURL = "https://www.funcraft.net/fr/"
 
 module.exports = {
 
     fetchPlayer: (username, period) => {
         return new Promise(async (resolve, reject) => {
-            const url = `${baseURL}joueurs?q=${username}`;
-            const res = await fetch(url);
-            const content = await res.text();
-            const $ = cheerio.load(content);
-            const error = $(".alert-danger").get(0);
-            if(!error) resolve(new Player($, period));
-                else reject("Player not found.");
-        });
+            const url = `${baseURL}joueurs?q=${username}`
+            const res = await fetch(url)
+            const content = await res.text()
+            const $ = cheerio.load(content)
+            const error = $(".alert-danger").get(0)
+            if(!error) resolve(new Player($, period))
+                else reject("Player not found.")
+        })
     },
 
     fetchLeaderboard: (game) => {
         return new Promise(async (resolve, reject) => {
-            const url = `${baseURL}classement/${game}/always`;
-            const res = await fetch(url);
-            const content = await res.text();
-            const $ = cheerio.load(content);
-            const error = $(".list-links").get(0);
-            if (!error) resolve(new Leaderboard($));
-                else reject("Game not found.");
-        });
+            const url = `${baseURL}classement/${game}/always`
+            const res = await fetch(url)
+            const content = await res.text()
+            const $ = cheerio.load(content)
+            const error = $(".list-links").get(0)
+            if (!error) resolve(new Leaderboard($))
+                else reject("Game not found.")
+        })
     },
 
     fetchPlayerCounts: () => {
         return new Promise(async (resolve) => {
-            const url = `${baseURL}jeux`;
-            const res = await fetch(url);
-            const content = await res.text();
-            const $ = cheerio.load(content);
-            resolve(new PlayerCounts($));
-        });
+            const url = `${baseURL}jeux`
+            const res = await fetch(url)
+            const content = await res.text()
+            const $ = cheerio.load(content)
+            resolve(new PlayerCounts($))
+        })
     }
 
-};
+}
